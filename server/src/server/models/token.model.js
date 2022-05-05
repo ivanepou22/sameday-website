78d4fb7f9823790c5c1b768cbc222e25db3ee49d
kallyas <kallyasmedia@gmail.com>
2022-05-05 13:03:25 +0300

    ch: add token model

---
+++ server/src/server/models/token.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import { toJson } from "./plugins.js";
+import { tokenTypes } from "../config/tokens.js";
+
+const tokenSchema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: [
+        tokenTypes.REFRESH,
+        tokenTypes.RESET_PASSWORD,
+        tokenTypes.VERIFY_EMAIL,
+      ],
+      required: true,
+    },
+    expires: {
+      type: Date,
+      required: true,
+    },
+    blacklisted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Token = mongoose.model("Token", tokenSchema);
+
+export default Token;
