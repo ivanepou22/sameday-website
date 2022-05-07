7974ff400a33697422395d9ccb46b11288ef1646
kallyas <kallyasmedia@gmail.com>
2022-05-07 11:39:17 +0300

    fix: db connection config

--- server/src/server/config/db.js
+++ server/src/server/config/db.js
@@ -2,29 +2,27 @@ import mongoose from 'mongoose'
 import chalk from 'chalk'
 import { DEVELOPMENT, TESTING, LOCAL, PRODUCTION } from './envTypes.js'
 
-let DB_URL
-
-switch (process.env.NODE_ENV) {
-    case DEVELOPMENT:
-        DB_URL = process.env.DB_URL_DEV
-        break;
-    case TESTING:
-        DB_URL = process.env.DB_URL_TEST
-        break;
-    case PRODUCTION:
-        DB_URL = process.env.DB_URL_PROD
-        break;
-    case LOCAL:
-        DB_URL = process.env.DB_URL_LOCAL
-        break;
-    default:
-        DB_URL = process.env.DB_URL_DEV
-        break;
-}
 
 async function DB(){
-    console.log(chalk.blue('Connecting to DB...'))
-    return mongoose.connect(process.env.DB_URL_DEV)
+let DB_URL;
+switch (process.env.NODE_ENV) {
+  case DEVELOPMENT:
+    DB_URL = process.env.DB_URL_DEV;
+    break;
+  case PRODUCTION:
+    DB_URL = process.env.DB_URL_PROD;
+    break;
+  case LOCAL:
+    DB_URL = process.env.DB_URL_LOC;
+    break;
+  case TESTING:
+    DB_URL = process.env.DB_URL_TEST;
+    break;
+  default:
+    DB_URL = process.env.DB_URL;
+}
+    console.log(chalk.blue('Connecting to DB using...' + process.env.NODE_ENV + ' environment'))
+    return mongoose.connect(DB_URL)
 }
 
 export default DB
