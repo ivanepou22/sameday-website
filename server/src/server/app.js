50480dd782e92ff7b7356b496e33b40530d42ee8
kallyas <kallyasmedia@gmail.com>
2022-05-10 15:53:14 +0300

    fix: imports and exports

--- server/src/server/app.js
+++ server/src/server/app.js
@@ -1,45 +1,53 @@
-import express from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
-import morgan from 'morgan';
-import xss from 'xss-clean';
-import compression from 'compression';
-import passport from 'passport';
-import cookieParser from 'cookie-parser';
-import mongoSanitize from 'express-mongo-sanitize';
-import httpStatus from 'http-status';
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import xss from "xss-clean";
+import compression from "compression";
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
+import httpStatus from "http-status";
+import config from "./config/config.js";
+import morgan from "./config/morgan.js";
+import { jwtStrategy } from "./config/passport.js";
+import authLimiter from "./middlewares/rateLimiter.js";
+import routes from "./routes/v1/index.js";
+import { errorConverter, errorHandler } from "./middlewares/error.js";
+import ApiError from "./utils/ApiError.js";
 
+const app = express();
 
-
-
-
-const app = express()
-
-if(process.env.NODE_ENV !== 'production'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
 }
 
-app.use(helmet())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(xss())
-app.use(mongoSanitize())
-app.use(compression())
-app.use(cors())
-app.options('*', cors())
-app.use(cookieParser())
-app.use(passport.initialize())
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(xss());
+app.use(mongoSanitize());
+app.use(compression());
+app.use(cors());
+app.options("*", cors());
+app.use(cookieParser());
+app.use(passport.initialize());
+passport.use("jwt", jwtStrategy);
 
+if (process.env.NODE_ENV === "production") {
+  app.use("/v1/auth", authLimiter);
+}
 
+app.use("/v1", routes);
 
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
 
+// convert error to ApiError, if needed
+app.use(errorConverter);
 
+// handle error
+app.use(errorHandler);
 
-
-
-
-
-
-
-
-export default app
+export default app;
