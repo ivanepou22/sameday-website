9e9ee2bf693c3adaf700ad4de53c50340a2b3e1d
kallyas <kallyasmedia@gmail.com>
2022-05-11 19:34:45 +0300

    ch: add user controller

--- server/src/server/controllers/auth.controller.js
+++ server/src/server/controllers/auth.controller.js
@@ -5,22 +5,14 @@ import { authService, userService, tokenService, emailService } from '../service
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
   const tokens = await tokenService.generateAuthTokens(user);
-  res.status(httpStatus.CREATED).send({ user: {
-    id: user._id,
-    email: user.email,
-    name: user.name,
-  }, tokens });
+  res.status(httpStatus.CREATED).send({user, tokens})
 });
 
 const login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
   const user = await authService.loginUserWithEmailAndPassword(email, password);
   const tokens = await tokenService.generateAuthTokens(user);
-  res.send({ user: {
-    id: user._id,
-    email: user.email,
-    name: user.name,
-  }, tokens });
+  res.send({ user, tokens });
 });
 
 const logout = catchAsync(async (req, res) => {
