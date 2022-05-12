b80a487084c86e689baa6108d29b32050c4fa9cf
kallyas <kallyasmedia@gmail.com>
2022-05-12 13:22:35 +0300

    ch: add order services

--- server/src/server/services/index.js
+++ server/src/server/services/index.js
@@ -2,5 +2,6 @@ import { authService } from "./auth.service.js";
 import { userService } from "./user.service.js";
 import { tokenService } from "./token.service.js";
 import { emailService } from "./email.service.js";
+import { orderService } from "./order.service.js";
 
-export { authService, userService, tokenService, emailService };
+export { authService, userService, tokenService, emailService, orderService };
