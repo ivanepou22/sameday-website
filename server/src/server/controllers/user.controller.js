94a7342806e90b3f6660a5adc43a2300293533ef
kallyas <kallyasmedia@gmail.com>
2022-05-12 13:22:09 +0300

    ch: add order controllers

--- server/src/server/controllers/user.controller.js
+++ server/src/server/controllers/user.controller.js
@@ -6,13 +6,7 @@ import { userService } from "../services/index.js";
 
 const createUser = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
-  res.status(httpStatus.CREATED).send({
-    user: {
-      id: user._id,
-      email: user.email,
-      name: user.name,
-    },
-  });
+  res.status(httpStatus.CREATED).send({ user });
 });
 
 const getUser = catchAsync(async (req, res) => {
@@ -20,13 +14,7 @@ const getUser = catchAsync(async (req, res) => {
   if (!user) {
     throw new ApiError(httpStatus.NOT_FOUND, "User not found");
   }
-  res.status(httpStatus.OK).send({
-    user: {
-      id: user._id,
-      email: user.email,
-      name: user.name,
-    },
-  });
+  res.status(httpStatus.OK).send({ user });
 });
 
 
