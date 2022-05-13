fb78e3ccd41ffed71c845c808329e59c173e65dd
kallyas <kallyasmedia@gmail.com>
2022-05-13 20:31:06 +0300

    ch: appointment, doctor controllers

---
+++ server/src/server/controllers/appointment.controller.js
@@ -0,0 +1,38 @@
+import httpStatus from "http-status";
+import pick from "../utils/pick.js";
+import catchAsync from "../utils/catchAsync.js";
+import ApiError from "../utils/ApiError.js";
+import { appointmentService } from "../services/index.js";
+
+
+const createAppointment = catchAsync(async (req, res) => {
+    const appointment = await appointmentService.createAppointment(req.body);
+    res.status(httpStatus.CREATED).send({ appointment });
+})
+
+const getAppointments = catchAsync(async (req, res) => {
+    const filter = pick(req.query, []);
+    const options = pick(req.query, ["sortBy", "limit", "page"]);
+    const result = await appointmentService.queryAppointments(filter, options);
+    res.status(httpStatus.OK).send(result);
+})
+
+const getAppointment = catchAsync(async (req, res) => {
+    const appointment = await appointmentService.getAppointmentById(req.params.appointmentId);
+    if (!appointment) {
+        throw new ApiError(httpStatus.NOT_FOUND, "Appointment not found");
+    }
+    res.status(httpStatus.OK).send({ appointment });
+})
+
+const updateAppointment = catchAsync(async (req, res) => {
+    const appointment = await appointmentService.updateAppointmentById(req.params.appointmentId, req.body);
+    res.send(appointment);
+})
+
+const deleteAppointment = catchAsync(async (req, res) => {
+    await appointmentService.deleteAppointmentById(req.params.appointmentId);
+    res.status(httpStatus.NO_CONTENT).send();
+})
+
+export { createAppointment, getAppointments, getAppointment, updateAppointment, deleteAppointment };
---
+++ server/src/server/controllers/doctor.controller.js
@@ -0,0 +1,38 @@
+import httpStatus from "http-status";
+import pick from "../utils/pick.js";
+import catchAsync from "../utils/catchAsync.js";
+import ApiError from "../utils/ApiError.js";
+import { doctorService } from "../services/index.js";
+
+const createDoctor = catchAsync(async (req, res) => {
+    const doctor = await doctorService.createDoctor(req.body);
+    res.status(httpStatus.CREATED).send({ doctor });
+})
+
+const getDoctors = catchAsync(async (req, res) => {
+    const filter = pick(req.query, ["name"]);
+    const options = pick(req.query, ["sortBy", "limit", "page"]);
+    const result = await doctorService.queryDoctors(filter, options);
+    res.status(httpStatus.OK).send(result);
+})
+
+const getDoctorById = catchAsync(async (req, res) => {
+    const doctor = await doctorService.getDoctorById(req.params.doctorId);
+    if (!doctor) {
+        throw new ApiError(httpStatus.NOT_FOUND, "Doctor not found");
+    }
+    res.status(httpStatus.OK).send({ doctor });
+})
+
+const updateDoctorById = catchAsync(async (req, res) => {
+    const doctor = await doctorService.updateDoctorById(req.params.doctorId, req.body);
+    res.send(doctor);
+})
+
+const deleteDoctorById = catchAsync(async (req, res) => {
+    await doctorService.deleteDoctorById(req.params.doctorId);
+    res.status(httpStatus.NO_CONTENT).send();
+})
+
+
+export { createDoctor, getDoctorById, getDoctors, updateDoctorById, deleteDoctorById };
