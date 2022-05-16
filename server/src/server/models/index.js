10bcb658b8badc507475760a40b9d9c676d9ba3d
kallyas <kallyasmedia@gmail.com>
2022-05-16 13:32:17 +0300

    ch: create service model

--- server/src/server/models/index.js
+++ server/src/server/models/index.js
@@ -4,6 +4,7 @@ import Order from "./order.model.js"
 import Appointment from "./appointment.model.js"
 import Doctor from "./doctor.model.js"
 import Patient from "./patient.model.js"
+import Service from "./service.model.js"
 
 
-export { Token, User, Order, Appointment, Doctor, Patient };
+export { Token, User, Order, Appointment, Doctor, Patient, Service };
---
+++ server/src/server/models/service.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { toJson, paginate } from "./plugins/index.js";
+
+const { Schema } = mongoose
+
+
+const serviceSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+}, {
+    timestamps: true
+})
+
+serviceSchema.plugin(toJson)
+serviceSchema.plugin(paginate)
+
+// add index to name, description, price, duration
+serviceSchema.index({ name: 'text', description: 'text', price: 'text', duration: 'text' })
+
+const Service = mongoose.model('Service', serviceSchema)
+
+export default Service
