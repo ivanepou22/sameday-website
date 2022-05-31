22d84905558c9bc1c4e71703507b8ce49cbe4071
kallyas <kallyasmedia@gmail.com>
2022-05-31 12:08:32 +0300

    update: add image field

--- server/src/server/models/doctor.model.js
+++ server/src/server/models/doctor.model.js
@@ -30,9 +30,8 @@ const doctorSchema = new Schema(
       trim: true,
     },
     specialities: {
-      type: String,
+      type: Array,
       required: true,
-      trim: true,
     },
   },
   { timestamps: true }
