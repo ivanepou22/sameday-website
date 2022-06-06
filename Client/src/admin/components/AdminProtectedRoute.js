c6daac0f856d6acbfb2a28e3391d635eac5bfe29
kallyas <kallyasmedia@gmail.com>
2022-06-06 14:29:49 +0300

    ch: add admin protected route

---
+++ Client/src/admin/components/AdminProtectedRoute.js
@@ -0,0 +1,16 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { authSelector } from "../../feautures/auth/authSlice";
+import { SDRoutes } from "../../routes";
+
+const AdminProtectedRoute = () => {
+  const { user } = useSelector(authSelector);
+
+  if (user && user.role !== "admin") {
+    return <Navigate to={SDRoutes.Home.path} />;
+  }
+
+  return user && user.role === "admin" ? <Outlet /> : <Navigate to="/admin" />;
+};
+
+export default AdminProtectedRoute
