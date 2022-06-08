9935fa7a352d59962ae1e4d0809632ec00c4c734
kallyas <kallyasmedia@gmail.com>
2022-06-08 12:11:18 +0300

    ch: update cart slice

--- Client/src/feautures/cart/cartSlice.js
+++ Client/src/feautures/cart/cartSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   cart: [],
   total: 0,
+  totalItems: 0,
 };
 
 const cartSlice = createSlice({
@@ -10,18 +11,38 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      const { cart, total } = state;
-      const { product } = action.payload;
-      const newCart = [...cart, product];
-      const newTotal = total + product.price;
-      return { cart: newCart, total: newTotal };
+      let { cart, total, totalItems } = state;
+      const product = action.payload;
+      // check if product already exists in cart and update quantity, price and total otherwise add new product
+      const productIndex = cart.findIndex((item) => item.id === product.id);
+      if (productIndex !== -1) {
+        cart[productIndex].quantity += 1;
+        cart[productIndex].total = cart[productIndex].quantity * cart[productIndex].price;
+      } else {
+        cart.push({ ...product, quantity: 1, total: product.price });
+      }
+      total += product.price;
+      totalItems += 1;
+      state.cart = cart;
+      state.total = total;
+      state.totalItems = totalItems;
     },
     removeFromCart: (state, action) => {
-      const { cart, total } = state;
-      const { product } = action.payload;
-      const newCart = cart.filter((item) => item.id !== product.id);
-      const newTotal = total - product.price;
-      return { cart: newCart, total: newTotal };
+      let { cart, total, totalItems } = state;
+      const product = action.payload;
+      const productIndex = cart.findIndex((item) => item.id === product.id);
+      if (productIndex !== -1) {
+        if (cart[productIndex].quantity > 1) {
+          cart[productIndex].quantity -= 1;
+          cart[productIndex].total = cart[productIndex].quantity * cart[productIndex].price;
+        } else {
+          cart.splice(productIndex, 1);
+        }
+        total -= product.price;
+      }
+      state.cart = cart;
+      state.total = total;
+      state.totalItems = totalItems - 1;
     },
     clearCart: (state) => {
       return { cart: [], total: 0 };
