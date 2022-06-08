33000123a7c54ccf0fec7bd49c292a60c582c9fb
Ivan Epou <ivanepou92@gmail.com>
2022-06-09 00:17:21 +0300

    Fx: fixed service details page

--- Client/src/components/ServiceDetailSection.jsx
+++ Client/src/components/ServiceDetailSection.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { getService, serviceSelector } from '../feautures/services/serviceSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import { AiOutlineClose } from 'react-icons/ai'
 import { BsCart, BsCartPlus } from 'react-icons/bs'
-import img1 from "../assets/images/pharmacy/shop/01.jpg"
 
 const ServiceDetailSection = () => {
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const { service } = useSelector(serviceSelector);
+
+
+    useEffect(() => {
+        dispatch(getService(id));
+    }, [dispatch, id]);
+
     return (
         <>
             <section className="section font-size-15 padding-top-20">
@@ -13,14 +22,14 @@ const ServiceDetailSection = () => {
                     <div className="row align-items-center">
                         <div className="col-md-5">
                             <div className="slider slider-for">
-                                <div><img src={img1} className="img-fluid rounded" alt="" /></div>
+                                <div><img src={service.image} className="img-fluid rounded" alt="" /></div>
                             </div>
                         </div>
 
                         <div className="col-md-7 mt-4 mt-sm-0 pt-2 pt-sm-0">
                             <div className="section-title ms-md-4">
-                                <h4 className="title">Plastic Medicine Box</h4>
-                                <h5 className="text-muted">$21.00 </h5>
+                                <h4 className="title">{service.name}</h4>
+                                <h5 className="text-muted">{service.price} </h5>
                                 <ul className="list-unstyled text-warning h5 mb-0">
                                     <li className="list-inline-item"><i className="mdi mdi-star"></i></li>
                                     <li className="list-inline-item"><i className="mdi mdi-star"></i></li>
@@ -31,7 +40,7 @@ const ServiceDetailSection = () => {
                                 </ul>
 
                                 <h5 className="mt-4 py-2">Overview :</h5>
-                                <p className="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero exercitationem, unde molestiae sint quae inventore atque minima natus fugiat nihil quisquam voluptates ea omnis. Modi laborum soluta tempore unde accusantium.</p>
+                                <p className="text-muted">{service.description}</p>
 
                                 <div className="d-flex shop-list align-items-center">
                                     <h6 className="mb-0">Quantity:</h6>
@@ -43,9 +52,9 @@ const ServiceDetailSection = () => {
                                 </div>
 
                                 <div className="mt-4 pt-2">
-                                    <Link to="/shop" className="btn btn-primary"><BsCartPlus /> Shop Now</Link>
-                                    <Link to="#" className="btn btn-soft-primary ms-2"><BsCart /> Add to Cart</Link>
-                                    <Link to="#" className="btn btn-red ms-2"> <AiOutlineClose /> Remove From Cart</Link>
+                                    <Link to="/shop" className="btn btn-primary mt-3"><BsCartPlus /> Shop Now</Link>
+                                    <Link to="#" className="btn btn-soft-primary ms-2 mt-3"><BsCart /> Add to Cart</Link>
+                                    <Link to="#" className="btn btn-red ms-2 mt-3"> <AiOutlineClose /> Remove From Cart</Link>
                                 </div>
                             </div>
                         </div>
