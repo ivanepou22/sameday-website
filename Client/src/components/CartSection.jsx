1cc5f85882412cee5607ec176b49a2fcc271a696
Ivan Epou <ivanepou92@gmail.com>
2022-06-09 11:43:06 +0300

    Fx: changing cart page design

--- Client/src/components/CartSection.jsx
+++ Client/src/components/CartSection.jsx
@@ -9,8 +9,8 @@ import {
   removeFromCart,
 } from "../feautures/cart/cartSlice";
 
-//images
-import img1 from "../assets/images/pharmacy/shop/ashwagandha.jpg";
+//icons
+import { FcEmptyTrash } from 'react-icons/fc'
 
 const CartSection = () => {
   const dispatch = useDispatch();
@@ -27,125 +27,138 @@ const CartSection = () => {
   return (
     <>
       <section className="section font-size-15 padding-top-20">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="table-responsive bg-white shadow rounded">
-                <table className="table table-center table-padding mb-0">
-                  <thead>
-                    <tr>
-                      <th className="border-bottom p-3" style={{ minWidth: "20px" }}></th>
-                      <th className="border-bottom p-3" style={{ minWidth: "300px" }}>
-                        Product
-                      </th>
-                      <th className="border-bottom text-center p-3" style={{ minWidth: "160px" }}>
-                        Price
-                      </th>
-                      <th className="border-bottom text-center p-3" style={{ minWidth: "190px" }}>
-                        Qty
-                      </th>
-                      <th className="border-bottom text-end p-3" style={{ minWidth: "50px" }}>
-                        Total
-                      </th>
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    {cart.length > 0 ? (
-                      cart.map((item) => (
-                        <tr key={item.id}>
-                          <td className="h5 p-3 text-center">
-                            <Link
-                              onClick={() => dispatch(clearItemFromCart(item))}
-                              to="#"
-                              className="text-danger"
-                            >
-                              <AiOutlineClose />
-                            </Link>
-                          </td>
-                          <td className="p-3">
-                            <div className="d-flex align-items-center">
-                              <img
-                                src={img1}
-                                className="img-fluid avatar avatar-small rounded shadow"
-                                style={{ height: "auto" }}
-                                alt=""
-                              />
-                              <h6 className="mb-0 ms-3">Ashwagandha Churna</h6>
-                            </div>
-                          </td>
-                          <td className="text-center p-3">UGX {item.price}</td>
-                          <td className="text-center shop-list p-3">
-                            <div className="qty-icons">
-                              <button
-                                onClick={() => handleRemove(item)}
-                                className="btn btn-icon btn-primary minus"
-                              >
-                                -
-                              </button>
-                              <input
-                                min="0"
-                                name="quantity"
-                                value={item.quantity}
-                                type="number"
-                                readOnly
-                                className="btn btn-icon btn-primary qty-btn quantity margin-left-right-10"
-                              />
-                              <button
-                                onClick={() => dispatch(addToCart(item))}
-                                className="btn btn-icon btn-primary plus"
-                              >
-                                +
-                              </button>
-                            </div>
-                          </td>
-                          <td className="text-end font-weight-bold p-3">UGX {item.total}</td>
+        {
+          cart.length > 0 ? (
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <div className="table-responsive bg-white shadow rounded">
+                    <table className="table table-center table-padding mb-0">
+                      <thead>
+                        <tr>
+                          <th className="border-bottom p-3" style={{ minWidth: "20px" }}></th>
+                          <th className="border-bottom p-3" style={{ minWidth: "300px" }}>
+                            Product
+                          </th>
+                          <th className="border-bottom text-center p-3" style={{ minWidth: "160px" }}>
+                            Price
+                          </th>
+                          <th className="border-bottom text-center p-3" style={{ minWidth: "190px" }}>
+                            Qty
+                          </th>
+                          <th className="border-bottom text-end p-3" style={{ minWidth: "50px" }}>
+                            Total
+                          </th>
                         </tr>
-                      ))
-                    ) : (
-                      <h1>Cart is Empty</h1>
-                    )}
-                  </tbody>
-                </table>
+                      </thead>
+
+                      <tbody>
+                        {
+                          cart.map((item) => (
+                            <tr key={item.id}>
+                              <td className="h5 p-3 text-center">
+                                <Link
+                                  onClick={() => dispatch(clearItemFromCart(item))}
+                                  to="#"
+                                  className="text-danger"
+                                >
+                                  <AiOutlineClose />
+                                </Link>
+                              </td>
+                              <td className="p-3">
+                                <div className="d-flex align-items-center">
+                                  <img
+                                    src={item?.image}
+                                    className="img-fluid avatar avatar-small rounded shadow"
+                                    style={{ height: "auto" }}
+                                    alt=""
+                                  />
+                                  <h6 className="mb-0 ms-3">{item?.name}</h6>
+                                </div>
+                              </td>
+                              <td className="text-center p-3">UGX {item.price}</td>
+                              <td className="text-center shop-list p-3">
+                                <div className="qty-icons">
+                                  <button
+                                    onClick={() => handleRemove(item)}
+                                    className="btn btn-icon btn-primary minus"
+                                  >
+                                    -
+                                  </button>
+                                  <input
+                                    min="0"
+                                    name="quantity"
+                                    value={item.quantity}
+                                    type="number"
+                                    readOnly
+                                    className="btn btn-icon btn-primary qty-btn quantity margin-left-right-10"
+                                  />
+                                  <button
+                                    onClick={() => dispatch(addToCart(item))}
+                                    className="btn btn-icon btn-primary plus"
+                                  >
+                                    +
+                                  </button>
+                                </div>
+                              </td>
+                              <td className="text-end font-weight-bold p-3">UGX {item.total}</td>
+                            </tr>
+                          ))
+                        }
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-lg-8 col-md-6 mt-4 pt-2">
+                  <Link to="/shop" className="btn btn-primary">
+                    Shop More
+                  </Link>
+                  <Link to="/shop" className="btn btn-soft-primary ms-2">
+                    Update Cart
+                  </Link>
+                </div>
+                <div className="col-lg-4 col-md-6 ms-auto mt-4 pt-2">
+                  <div className="table-responsive bg-white rounded shadow">
+                    <table className="table table-center table-padding mb-0">
+                      <tbody>
+                        <tr>
+                          <td className="h6 p-3">Subtotal</td>
+                          <td className="text-end font-weight-bold p-3">UGX {totalPrice}</td>
+                        </tr>
+                        <tr>
+                          <td className="h6 p-3">Taxes</td>
+                          <td className="text-end font-weight-bold p-3">UGX 0</td>
+                        </tr>
+                        <tr className="bg-light">
+                          <td className="h6 p-3">Total</td>
+                          <td className="text-end font-weight-bold p-3">UGX {totalPrice}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div className="mt-4 pt-2 text-end">
+                    <Link to="/checkout" className="btn btn-primary">
+                      Proceed to checkout
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-8 col-md-6 mt-4 pt-2">
-              <Link to="/shop" className="btn btn-primary">
-                Shop More
-              </Link>
-              <Link to="/shop" className="btn btn-soft-primary ms-2">
-                Update Cart
-              </Link>
-            </div>
-            <div className="col-lg-4 col-md-6 ms-auto mt-4 pt-2">
-              <div className="table-responsive bg-white rounded shadow">
-                <table className="table table-center table-padding mb-0">
-                  <tbody>
-                    <tr>
-                      <td className="h6 p-3">Subtotal</td>
-                      <td className="text-end font-weight-bold p-3">UGX {totalPrice}</td>
-                    </tr>
-                    <tr>
-                      <td className="h6 p-3">Taxes</td>
-                      <td className="text-end font-weight-bold p-3">UGX 0</td>
-                    </tr>
-                    <tr className="bg-light">
-                      <td className="h6 p-3">Total</td>
-                      <td className="text-end font-weight-bold p-3">UGX {totalPrice}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div className="mt-4 pt-2 text-end">
-                <Link to="/checkout" className="btn btn-primary">
-                  Proceed to checkout
-                </Link>
+          ) : (
+            <div className="container">
+              <div className="row">
+                <div className="cart-empty">
+                  <div className="empty-bin">
+                    <FcEmptyTrash />
+                  </div>
+                  <span>Your Cart is Empty</span>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          )
+        }
       </section>
     </>
   );
