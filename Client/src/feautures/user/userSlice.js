6e84382f4b0b54df3676e9dce03b1b9ab2899e7b
kallyas <kallyasmedia@gmail.com>
2022-06-09 21:40:25 +0300

    ch: add user slice

---
+++ Client/src/feautures/user/userSlice.js
@@ -0,0 +1,97 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { userService } from "../../services/userService";
+
+
+const initialState = {
+    users: [],
+    isLoading: false,
+    isError: false,
+    errorMessage: ""
+}
+
+export const userSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.users = [];
+            state.isLoading = false;
+            state.isError = false;
+            state.errorMessage = "";
+        },
+    },
+    extraReducers: builder => {
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.users = action.payload;
+            state.isLoading = false;
+        });
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.errorMessage = action.payload;
+        });
+        builder.addCase(updateUser.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(updateUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.users = state.users.map(user => {
+                if (user.id === action.payload.id) {
+                    return action.payload;
+                }
+                return user;
+            });
+        });
+        builder.addCase(updateUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.errorMessage = action.payload;
+        });
+        builder.addCase(deleteUser.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(deleteUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.users = state.users.filter(user => user.id !== action.payload);
+        });
+        builder.addCase(deleteUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.errorMessage = action.payload;
+        });
+    }
+});
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async (_, { rejectWithValue}) => {
+    try {
+        const users = await userService.getUsers();
+        return users;
+    } catch (error) {
+        return rejectWithValue(error.message);
+    }
+})
+
+export const updateUser = createAsyncThunk("users/updateUser", async (user, { rejectWithValue }) => {
+    try {
+        const updatedUser = await userService.updateUser(user);
+        return updatedUser;
+    } catch (error) {
+        return rejectWithValue(error.message);
+    }
+})
+
+
+export const deleteUser = createAsyncThunk("users/deleteUser", async (id, { rejectWithValue }) => {
+    try {
+        await userService.deleteUser(id);
+    } catch (error) {
+        return rejectWithValue(error.message);
+    }
+})
+
+export const { reset } = userSlice.actions;
+export const userSelector = state => state.users;
+export default userSlice.reducer;
