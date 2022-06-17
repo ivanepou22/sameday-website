58ed8430c193d7021ec514ef3c3ca25f57d895ae
kallyas <kallyasmedia@gmail.com>
2022-06-17 16:15:07 +0300

    ch: add patient modal functionality

--- Client/src/admin/components/PatientModal.jsx
+++ Client/src/admin/components/PatientModal.jsx
@@ -1,96 +1,228 @@
-import React from 'react'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { imageSelector, UploadImage } from "../../feautures/image/imageSlice";
+import { generatePatientNumber } from "../../utils/utils";
 
 const PatientModal = (props) => {
-    const { show, setShowModal } = props;
+  const { show, setShowModal } = props;
+  const dispatch = useDispatch();
+  const { imageUrl } = useSelector(imageSelector);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    patientNumber: generatePatientNumber(),
+    gender: "",
+    dateOfBirth: "",
+    email: "",
+    phone: "",
+    address: "",
+  });
 
-    const handleClose = () => {
-        setShowModal(false);
-    }
+  const handleClose = () => {
+    setShowModal(false);
+  };
 
-    return (
-        <>
-            <div className={`modal modal-blur fade bg-gray ${show ? 'display-block show' : 'display-none'}`} id="appointmentform" tabIndex="-1" onClick={handleClose} aria-labelledby="exampleModalLabel" aria-hidden={!show} aria-modal={show} role={show ? 'dialog' : ''} >
-                <div className="modal-dialog modal-lg modal-dialog-centered" onClick={e => { e.stopPropagation(); }} >
-                    <div className="modal-content">
-                        <div className="modal-header border-bottom p-3">
-                            <h5 className="modal-title" id="exampleModalLabel">Add a Patient</h5>
-                            <button type="button" onClick={handleClose} className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body p-3 pt-4">
-                            <form>
-                                <div className="row">
-                                    <div className="col-lg-6 col-md-6">
-                                        <div className="mb-3">
-                                            <label className="form-label">Patient Number <span className="text-danger">*</span></label>
-                                            <input name="pNumber" id="pNumber" type="text" className="form-control" placeholder="Patient Number :" />
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 col-md-6">
-                                        <div className="mb-3">
-                                            <label className="form-label">First Name <span className="text-danger">*</span></label>
-                                            <input name="f_name" id="f_name" type="text" className="form-control" placeholder="Your First Name :" />
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 col-md-6">
-                                        <div className="mb-3">
-                                            <label className="form-label">Last Name <span className="text-danger">*</span></label>
-                                            <input name="f_name" id="f_name" type="text" className="form-control" placeholder="Your Last Name :" />
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 col-md-6">
-                                        <div className="mb-3">
-                                            <label className="form-label">Gender <span className="text-danger">*</span></label>
-                                            <select className="form-control doctor-name select2input">
-                                                <option defaultValue="">Select Gender</option>
-                                                <option defaultValue="female">Female</option>
-                                                <option defaultValue="male">Male</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 col-md-6">
-                                        <div className="mb-3">
-                                            <label className="form-label"> Date of Birth : <span className="text-danger">*</span></label>
-                                            <input name="date" type="date" className="flatpickr flatpickr-input form-control" id="checkin-date" />
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 col-md-6">
-                                        <div className="mb-3">
-                                            <label className="form-label">Your Email <span className="text-danger">*</span></label>
-                                            <input name="email" id="email" type="email" className="form-control" placeholder="Your email :" />
-                                        </div>
-                                    </div>
+  const handleImageUpload = (e) => {
+    const ps = new Promise((resolve, reject) => {
+      const data = new FormData();
+      data.append("file", e.target.files[0]);
+      data.append("upload_preset", "ggikucjk");
+      resolve(dispatch(UploadImage(data)));
+    });
+    toast.promise(ps, {
+      pending: "Uploading...",
+      success: "Image uploaded successfully",
+      error: "Image upload failed",
+    });
+  };
 
-                                    <div className="col-lg-6 col-md-6">
-                                        <div className="mb-3">
-                                            <label className="form-label">Your Phone <span className="text-danger">*</span></label>
-                                            <input name="phone" id="phone" type="tel" className="form-control" placeholder="Your Phone :" />
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 col-md-6">
-                                        <div className="mb-3">
-                                            <label className="form-label">Image <span className="text-danger">*</span></label>
-                                            <input name="image" id="image" type="file" className="form-control" placeholder="Your Image :" />
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-12">
-                                        <div className="mb-3">
-                                            <label className="form-label">Address <span className="text-danger">*</span></label>
-                                            <textarea name="address" id="address" rows="3" className="form-control" placeholder="Address :"></textarea>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-12">
-                                        <div className="float-right">
-                                            <button type="submit" className="btn btn-primary">Submit a Patient</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //   dispatch(createPatient())
+  };
+
+  return (
+    <>
+      <div
+        className={`modal modal-blur fade bg-gray ${show ? "display-block show" : "display-none"}`}
+        id="appointmentform"
+        tabIndex="-1"
+        onClick={handleClose}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden={!show}
+        aria-modal={show}
+        role={show ? "dialog" : ""}
+      >
+        <div
+          className="modal-dialog modal-lg modal-dialog-centered"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <div className="modal-content">
+            <div className="modal-header border-bottom p-3">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Add a Patient
+              </h5>
+              <button
+                type="button"
+                onClick={handleClose}
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
-        </>
-    )
-}
+            <div className="modal-body p-3 pt-4">
+              <form>
+                <div className="row">
+                  <div className="col-lg-6 col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">
+                        Patient Number <span className="text-danger">*</span>
+                      </label>
+                      <input
+                        name="pNumber"
+                        id="pNumber"
+                        type="text"
+                        className="form-control"
+                        placeholder="Patient Number :"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">
+                        First Name <span className="text-danger">*</span>
+                      </label>
+                      <input
+                        name="f_name"
+                        id="f_name"
+                        type="text"
+                        className="form-control"
+                        placeholder="Your First Name :"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">
+                        Last Name <span className="text-danger">*</span>
+                      </label>
+                      <input
+                        name="f_name"
+                        id="f_name"
+                        type="text"
+                        className="form-control"
+                        placeholder="Your Last Name :"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">
+                        Gender <span className="text-danger">*</span>
+                      </label>
+                      <select className="form-control doctor-name select2input">
+                        <option defaultValue="">Select Gender</option>
+                        <option defaultValue="female">Female</option>
+                        <option defaultValue="male">Male</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">
+                        {" "}
+                        Date of Birth : <span className="text-danger">*</span>
+                      </label>
+                      <input
+                        name="date"
+                        type="date"
+                        className="flatpickr flatpickr-input form-control"
+                        id="checkin-date"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">
+                        Your Email <span className="text-danger">*</span>
+                      </label>
+                      <input
+                        name="email"
+                        id="email"
+                        type="email"
+                        className="form-control"
+                        placeholder="Your email :"
+                      />
+                    </div>
+                  </div>
 
-export default PatientModal
+                  <div className="col-lg-6 col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">
+                        Your Phone <span className="text-danger">*</span>
+                      </label>
+                      <input
+                        name="phone"
+                        id="phone"
+                        type="tel"
+                        className="form-control"
+                        placeholder="Your Phone :"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">
+                        Image <span className="text-danger">*</span>
+                      </label>
+                      <input
+                        name="image"
+                        id="image"
+                        type="file"
+                        className="form-control"
+                        placeholder="Your Image :"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-12">
+                    <div className="mb-3">
+                      <label className="form-label">
+                        Address <span className="text-danger">*</span>
+                      </label>
+                      <textarea
+                        name="address"
+                        id="address"
+                        rows="3"
+                        className="form-control"
+                        placeholder="Address :"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="col-lg-12">
+                    <div className="float-right">
+                      <button type="submit" className="btn btn-primary">
+                        Submit a Patient
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PatientModal;
--- Client/src/admin/components/PatientTable.jsx
+++ Client/src/admin/components/PatientTable.jsx
@@ -1,64 +1,90 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { RiAddLine } from 'react-icons/ri'
-import Table from './Table'
-import PatientModal from './PatientModal'
+import React from "react";
+import { Link } from "react-router-dom";
+import { RiAddLine } from "react-icons/ri";
+import Table from "./Table";
+import PatientModal from "./PatientModal";
 
 const PatientTable = (props) => {
-    const { users } = props
-    const [showModal, setShowModal] = React.useState(false);
+  const { users } = props;
+  const [showModal, setShowModal] = React.useState(false);
 
-    const handleShowModal = () => {
-        setShowModal(true);
-    }
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
 
-    return (
-        <>
-            <div className="container-fluid">
-                <div className="layout-specing">
-                    <div className="row">
-                        <div className="col-xl-9 col-md-6">
-                            <h5 className="mb-0">Patients</h5>
-                            <nav aria-label="breadcrumb" className="d-inline-block mt-2">
-                                <ul className="breadcrumb breadcrumb-muted bg-transparent rounded mb-0 p-0">
-                                    <li className="breadcrumb-item"><Link to="/admin/dashboard">SameDay Lab Works</Link></li>
-                                    <li className="breadcrumb-item active" aria-current="page">Patients</li>
-                                </ul>
-                            </nav>
-                        </div>
-
-                        <div className="col-xl-3 col-md-6 mt-4 mt-md-0 text-md-end">
-                            <Link to='#/' onClick={handleShowModal} className="btn btn-primary"><RiAddLine /> Add patient</Link>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-12 mt-4">
-                            <div className="table-responsive shadow rounded">
-                                <Table data={users} />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="row text-center">
-                        <div className="col-12 mt-4">
-                            <div className="d-md-flex align-items-center text-center justify-content-between">
-                                <span className="text-muted me-3">Showing 1 - 10 out of 50</span>
-                                <ul className="pagination justify-content-center mb-0 mt-3 mt-sm-0">
-                                    <li className="page-item"><Link className="page-link" to="#/" aria-label="Previous">Prev</Link></li>
-                                    <li className="page-item active"><Link className="page-link" to="#/">1</Link></li>
-                                    <li className="page-item"><Link className="page-link" to="#/">2</Link></li>
-                                    <li className="page-item"><Link className="page-link" to="#/">3</Link></li>
-                                    <li className="page-item"><Link className="page-link" to="#/" aria-label="Next">Next</Link></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="layout-specing">
+          <div className="row">
+            <div className="col-xl-9 col-md-6">
+              <h5 className="mb-0">Patients</h5>
+              <nav aria-label="breadcrumb" className="d-inline-block mt-2">
+                <ul className="breadcrumb breadcrumb-muted bg-transparent rounded mb-0 p-0">
+                  <li className="breadcrumb-item">
+                    <Link to="/admin/dashboard">SameDay Lab Works</Link>
+                  </li>
+                  <li className="breadcrumb-item active" aria-current="page">
+                    Patients
+                  </li>
+                </ul>
+              </nav>
             </div>
-            <PatientModal show={showModal} setShowModal={setShowModal} />
-        </>
-    )
-}
 
-export default PatientTable
+            <div className="col-xl-3 col-md-6 mt-4 mt-md-0 text-md-end">
+              <Link to="#/" onClick={handleShowModal} className="btn btn-primary">
+                <RiAddLine /> Add patient
+              </Link>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12 mt-4">
+              <div className="table-responsive shadow rounded">
+                <Table data={users} />
+              </div>
+            </div>
+          </div>
+
+          <div className="row text-center">
+            <div className="col-12 mt-4">
+              <div className="d-md-flex align-items-center text-center justify-content-between">
+                <span className="text-muted me-3">Showing 1 - 10 out of 50</span>
+                <ul className="pagination justify-content-center mb-0 mt-3 mt-sm-0">
+                  <li className="page-item">
+                    <Link className="page-link" to="#/" aria-label="Previous">
+                      Prev
+                    </Link>
+                  </li>
+                  <li className="page-item active">
+                    <Link className="page-link" to="#/">
+                      1
+                    </Link>
+                  </li>
+                  <li className="page-item">
+                    <Link className="page-link" to="#/">
+                      2
+                    </Link>
+                  </li>
+                  <li className="page-item">
+                    <Link className="page-link" to="#/">
+                      3
+                    </Link>
+                  </li>
+                  <li className="page-item">
+                    <Link className="page-link" to="#/" aria-label="Next">
+                      Next
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <PatientModal show={showModal} setShowModal={setShowModal} />
+    </>
+  );
+};
+
+export default PatientTable;
