1047300162cdb39a624b3e6e43c5e77304665722
kallyas <kallyasmedia@gmail.com>
2022-07-07 18:23:06 +0300

    ch: send email

--- server/src/server/controllers/email.controller.js
+++ server/src/server/controllers/email.controller.js
@@ -3,8 +3,13 @@ import catchAsync from "../utils/catchAsync.js";
 import { emailService } from "../services/email.service.js";
 
 const sendContactEmail = catchAsync(async (req, res) => {
-  const { name, email, message } = req.body;
-  await emailService.sendContactEmail(name, email, message);
+  const { name, email, message, subject } = req.body;
+  const text = {
+    name,
+    email,
+    message,
+  };
+  await emailService.sendContactEmail(subject, text);
   res.status(httpStatus.NO_CONTENT).send();
 });
 
