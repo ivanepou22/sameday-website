ca6ea29b830b8da9f1c82bb19963a2aa243df481
kallyas <kallyasmedia@gmail.com>
2022-08-06 23:14:17 +0300

    update: order validation to match order model

--- server/src/server/validations/order.validation.js
+++ server/src/server/validations/order.validation.js
@@ -2,55 +2,60 @@ import Joi from "joi";
 import { objectId } from "./custom.validation.js";
 
 const createOrder = {
-    body: Joi.object().keys({
-        userId: Joi.string().custom(objectId),
-        orderDate: Joi.date().required(),
-        orderItems: Joi.array().items(
-            Joi.object().keys({
-                itemId: Joi.string().custom(objectId).required(),
-                itemQuantity: Joi.number().required(),
-            })
-        ).required(),
-        orderTotal: Joi.number().required(),
-    }),
+  body: Joi.object().keys({
+    orderNumber: Joi.string().required(),
+    userId: Joi.string().custom(objectId),
+    orderDate: Joi.date().required(),
+    orderItems: Joi.array()
+      .items(
+        Joi.object().keys({
+          itemId: Joi.string().custom(objectId).required(),
+          itemQuantity: Joi.number().required(),
+          itemName: Joi.string().required(),
+          itemPrice: Joi.number().required(),
+        })
+      )
+      .required(),
+    orderTotal: Joi.number().required(),
+    homeService: Joi.boolean().required(),
+  }),
 };
 
 const getOrders = {
-    query: Joi.object().keys({
-        sortBy: Joi.string(),
-        limit: Joi.number().integer(),
-        page: Joi.number().integer(),
-    }),
+  query: Joi.object().keys({
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
 };
 
 const getOrder = {
-    params: Joi.object().keys({
-        orderId: Joi.string().custom(objectId),
-    }),
+  params: Joi.object().keys({
+    orderId: Joi.string().custom(objectId),
+  }),
 };
 
 const updateOrder = {
-    params: Joi.object().keys({
-        orderId: Joi.required().custom(objectId),
-    }),
-    body: Joi.object().keys({
-        orderStatus: Joi.string().valid("pending", "approved", "rejected", "delivered"),
-    }),
+  params: Joi.object().keys({
+    orderId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object().keys({
+    orderStatus: Joi.string().valid("pending", "approved", "rejected", "delivered"),
+  }),
 };
 
 const deleteOrder = {
-    params: Joi.object().keys({
-        orderId: Joi.string().custom(objectId),
-    }),
+  params: Joi.object().keys({
+    orderId: Joi.string().custom(objectId),
+  }),
 };
 
 const orderValidation = {
-    createOrder,
-    getOrders,
-    getOrder,
-    updateOrder,
-    deleteOrder,
+  createOrder,
+  getOrders,
+  getOrder,
+  updateOrder,
+  deleteOrder,
 };
-
 
 export default orderValidation;
