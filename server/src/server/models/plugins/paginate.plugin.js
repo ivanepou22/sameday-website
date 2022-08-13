9fd9843856df19582a99418a6e8e017d7866bc39
kallyas <kallyasmedia@gmail.com>
2022-08-13 14:22:15 +0300

    fix: pagination on the server

--- server/src/server/models/plugins/paginate.plugin.js
+++ server/src/server/models/plugins/paginate.plugin.js
@@ -12,8 +12,10 @@ const paginate = (schema) => {
       sort = 'createdAt';
     }
 
-    const limit = options.limit && parseInt(options.limit, 10) > 10 ? parseInt(options.limit, 10) : 10;
-    const page = options.page && parseInt(options.page, 10) > 10 ? parseInt(options.page, 10) : 1;
+    console.log(options)
+
+    const limit = options.limit && parseInt(options.limit, 10) > 0 ? parseInt(options.limit, 10) : 10;
+    const page = options.page && parseInt(options.page, 10) > 0 ? parseInt(options.page, 10) : 1;
     const skip = (page - 1) * limit;
 
     const countPromise = this.countDocuments(filter).exec();
