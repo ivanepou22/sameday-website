2e091a0192d0f4730640af5026701dcd91660cac
kallyas <kallyasmedia@gmail.com>
2022-08-13 14:28:30 +0300

    fix: remove logs

--- server/src/server/models/plugins/paginate.plugin.js
+++ server/src/server/models/plugins/paginate.plugin.js
@@ -12,8 +12,6 @@ const paginate = (schema) => {
       sort = 'createdAt';
     }
 
-    console.log(options)
-
     const limit = options.limit && parseInt(options.limit, 10) > 0 ? parseInt(options.limit, 10) : 10;
     const page = options.page && parseInt(options.page, 10) > 0 ? parseInt(options.page, 10) : 1;
     const skip = (page - 1) * limit;
