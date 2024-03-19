61fa5510e2f6b53cf508d9f4157c9165f5bdd090
kallyas <kallyasmedia@gmail.com>
2024-03-19 20:37:12 +0300

    Refactor ContactInfo component

--- frontend/src/components/ContactInfo.jsx
+++ frontend/src/components/ContactInfo.jsx
@@ -1,75 +1,93 @@
 /* eslint-disable */
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { FiPhone, } from 'react-icons/fi'
-import { BiEnvelope } from 'react-icons/bi'
-import { AiOutlineEnvironment } from 'react-icons/ai'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPhone } from "react-icons/fi";
+import { BiEnvelope } from "react-icons/bi";
+import { AiOutlineEnvironment } from "react-icons/ai";
 
 const ContactInfo = () => {
-    return (
-        <>
-            <div className="container font-size-15">
-                <div className="row">
-                    <div className="col-lg-4 col-md-6">
-                        <div className="card features feature-primary text-center border-0">
-                            <div className="icon text-center rounded-md mx-auto bg-light-orange">
-                                <FiPhone className='font-size-20' />
-                            </div>
-                            <div className="card-body p-0 mt-3">
-                                <h5>Phone</h5>
-                                <p className="text-muted mt-3 align-text-center">Get in Touch on Phone</p>
-                                <p>
-                                    <Link to="tel:+256708850000" className="link">+256708850000 - Entebbe</Link>
-                                </p>
-                                <p>
-                                    <Link to="tel:+256775108946" className="link">+256775108946 - Kampala</Link>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-4 col-md-6 mt-4 mt-sm-0 pt-2 pt-sm-0">
-                        <div className="card features feature-primary text-center border-0">
-                            <div className="icon text-center rounded-md mx-auto bg-light-orange">
-                                <BiEnvelope className='font-size-20' />
-                            </div>
-                            <div className="card-body p-0 mt-3 align-text-center">
-                                <h5>Email</h5>
-                                <p className="text-muted mt-3"> Get in Touch on Email</p>
-                                <Link to="mailto:info@samedaylabwork.com" className="link">info@samedaylabwork.com</Link>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-4 col-md-6 mt-4 mt-lg-0 pt-2 pt-lg-0">
-                        <div className="card features feature-primary text-center border-0">
-                            <div className="icon text-center rounded-md mx-auto">
-                                <AiOutlineEnvironment className='font-size-20' />
-                            </div>
-                            <div className="card-body p-0 mt-3">
-                                <h5>Locations</h5>
-                                <p className="text-muted mt-3">
-                                    <Link to="https://www.google.com/maps/@0.3311533,32.6025039,3a,90y,72.1h,86.9t/data=!3m6!1e1!3m4!1sfYmrwFrEby97rrxgVVL1hQ!2e0!7i13312!8i6656?hl=en" className="link">
-                                        Imperial Mall, Shop No L29, Lower Ground Floor, Lugard Avenue – Entebbe
-                                    </Link>
-                                </p>
-                                <p className="text-muted mt-3">
-                                    <Link to="https://www.google.com/maps/@0.3311533,32.6025039,3a,75y,166.64h,87.08t,359.4r/data=!3m6!1e1!3m4!1sfYmrwFrEby97rrxgVVL1hQ!2e0!7i13312!8i6656?hl=en" className="link">
-                                        REGENCY PLAZA, Office No 3, 18 Lugogo By-Pass – Kampala
-                                    </Link>
-                                </p>
-                                <p className="text-muted mt-3">
-                                    <Link to="https://www.google.com/maps/place/Peniel+Beach+Hotel/@0.0395145,32.4596532,18z/data=!4m8!3m7!1s0x177d86c52afdeed7:0xf99d53c25a719bcc!5m2!4m1!1i2!8m2!3d0.0391212!4d32.4593148?hl=en" className="link">
-                                        Peniel Beach Hotel, Airport Road - Airport, Entebbe
-                                    </Link>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container font-size-15">
+        <div className="row">
+          <div className="col-lg-4 col-md-6">
+            <div className="card features feature-primary text-center border-0">
+              <div className="icon text-center rounded-md mx-auto bg-light-orange">
+                <FiPhone className="font-size-20" />
+              </div>
+              <div className="card-body p-0 mt-3">
+                <h5>Phone</h5>
+                <p className="text-muted mt-3 align-text-center">
+                  Get in Touch on Phone
+                </p>
+                <p>
+                  <Link to="tel:+256708850000" className="link">
+                    +256708850000 - Entebbe
+                  </Link>
+                </p>
+                <p>
+                  <Link to="tel:+256775108946" className="link">
+                    +256775108946 - Kampala
+                  </Link>
+                </p>
+              </div>
             </div>
-        </>
-    )
-}
+          </div>
 
-export default ContactInfo
+          <div className="col-lg-4 col-md-6 mt-4 mt-sm-0 pt-2 pt-sm-0">
+            <div className="card features feature-primary text-center border-0">
+              <div className="icon text-center rounded-md mx-auto bg-light-orange">
+                <BiEnvelope className="font-size-20" />
+              </div>
+              <div className="card-body p-0 mt-3 align-text-center">
+                <h5>Email</h5>
+                <p className="text-muted mt-3"> Get in Touch on Email</p>
+                <Link to="mailto:info@samedaylabwork.com" className="link">
+                  info@samedaylabwork.com
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 mt-4 mt-lg-0 pt-2 pt-lg-0">
+            <div className="card features feature-primary text-center border-0">
+              <div className="icon text-center rounded-md mx-auto">
+                <AiOutlineEnvironment className="font-size-20" />
+              </div>
+              <div className="card-body p-0 mt-3">
+                <h5>Locations</h5>
+                <p className="text-muted mt-3">
+                  <Link
+                    to="https://www.google.com/maps/@0.3311533,32.6025039,3a,90y,72.1h,86.9t/data=!3m6!1e1!3m4!1sfYmrwFrEby97rrxgVVL1hQ!2e0!7i13312!8i6656?hl=en"
+                    className="link"
+                  >
+                    Imperial Mall, Shop No L29, Lower Ground Floor, Lugard
+                    Avenue – Entebbe
+                  </Link>
+                </p>
+                <p className="text-muted mt-3">
+                  <Link
+                    to="https://www.google.com/maps/@0.3311533,32.6025039,3a,75y,166.64h,87.08t,359.4r/data=!3m6!1e1!3m4!1sfYmrwFrEby97rrxgVVL1hQ!2e0!7i13312!8i6656?hl=en"
+                    className="link"
+                  >
+                    REGENCY PLAZA, Office No 3, 18 Lugogo By-Pass – Kampala
+                  </Link>
+                </p>
+                <p className="text-muted mt-3">
+                  <Link
+                    to="https://www.google.com/maps/place/Peniel+Beach+Hotel/@0.0395145,32.4596532,18z/data=!4m8!3m7!1s0x177d86c52afdeed7:0xf99d53c25a719bcc!5m2!4m1!1i2!8m2!3d0.0391212!4d32.4593148?hl=en"
+                    className="link"
+                  >
+                    Peniel Beach Hotel, Airport Road - Airport, Entebbe
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactInfo;
--- frontend/src/components/CovidContent.jsx
+++ frontend/src/components/CovidContent.jsx
@@ -1,107 +1,153 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {AiOutlineSafetyCertificate} from 'react-icons/ai'
-import {SiPytest} from 'react-icons/si'
-import {RiServiceLine, RiHomeGearLine, RiHotelBedLine} from 'react-icons/ri'
-import {FaWalking   } from 'react-icons/fa'
+import React from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
+import { SiPytest } from "react-icons/si";
+import { RiServiceLine, RiHomeGearLine, RiHotelBedLine } from "react-icons/ri";
+import { FaWalking } from "react-icons/fa";
 
 const CovidContent = () => {
-    return (
-        <section className="section about-section font-size-15 padding-bottom-20">
-            <div className="container">
-            <div className="row justify-content-center">
-                        <div className="col-12">
-                            <div className="section-title mb-4 pb-2 text-center">
-                                <h4 className="choose-title mb-4 text-orange">COVID Testing Services</h4>
-                                <p className="text-muted mx-auto para-desc mb-0">
-                                    We perform advanced Polymerase Chain Reaction (PCR) and PCR-SARS-CoV-2
-                                    rapid diagnostic testing on travelers, the general public, and on behalf of private institutions.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                <div className="row">
-                    <div className="col-xl-4 col-md-4 col-12 mt-5">
-                        <div className="card features feature-primary border-0">
-                            <div className="icon text-center rounded-md icon-font">
-                                <AiOutlineSafetyCertificate />
-                            </div>
-                            <div className="card-body p-0 mt-3">
-                                <Link to="#/" className="title text-dark h5">Flying Certificate</Link>
-                                <p className="text-muted mt-3">Get fast, accurate results and your flying certificate with enough time to spare for your flight.</p>
-                                <Link to="#/" className="link">Read More <i className="ri-arrow-right-line align-middle"></i></Link>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-xl-4 col-md-4 col-12 mt-5">
-                        <div className="card features feature-primary border-0">
-                            <div className="icon text-center rounded-md icon-font">
-                                <SiPytest />
-                            </div>
-                            <div className="card-body p-0 mt-3">
-                                <Link to="#/" className="title text-dark h5">PCR Testing</Link>
-                                <p className="text-muted mt-3">The gold standard of covid-19 testing with a high level of professionalism and expertise. Come and get to know your results.</p>
-                                <Link to="#/" className="link">Read More <i className="ri-arrow-right-line align-middle"></i></Link>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-xl-4 col-md-4 col-12 mt-5">
-                        <div className="card features feature-primary border-0">
-                            <div className="icon text-center rounded-md icon-font">
-                                <RiServiceLine />
-                            </div>
-                            <div className="card-body p-0 mt-3">
-                                <Link to="#/" className="title text-dark h5">Same Day Service</Link>
-                                <p className="text-muted mt-3">Need to travel today? Get your results and certificate to fly on the same day before your flight!</p>
-                                <Link to="#/" className="link">Read More <i className="ri-arrow-right-line align-middle"></i></Link>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-xl-4 col-md-4 col-12 mt-5">
-                        <div className="card features feature-primary border-0">
-                            <div className="icon text-center rounded-md icon-font">
-                                <RiHomeGearLine />
-                            </div>
-                            <div className="card-body p-0 mt-3">
-                                <Link to="#/" className="title text-dark h5">Home Testing Service</Link>
-                                <p className="text-muted mt-3">How convenient can it get? We come to your home, take your sample and send you the results. Sweet!</p>
-                                <Link to="#/" className="link">Read More <i className="ri-arrow-right-line align-middle"></i></Link>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-xl-4 col-md-4 col-12 mt-5">
-                        <div className="card features feature-primary border-0">
-                            <div className="icon text-center rounded-md icon-font">
-                                <RiHotelBedLine />
-                            </div>
-                            <div className="card-body p-0 mt-3">
-                                <Link to="#/" className="title text-dark h5">Hotel Testing Service</Link>
-                                <p className="text-muted mt-3">Schedule your covid-19 test with the front desk in the convenience of your hotel room and we will come to you !</p>
-                                <Link to="#/" className="link">Read More <i className="ri-arrow-right-line align-middle"></i></Link>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-xl-4 col-md-4 col-12 mt-5">
-                        <div className="card features feature-primary border-0">
-                            <div className="icon text-center rounded-md icon-font">
-                                <FaWalking />
-                            </div>
-                            <div className="card-body p-0 mt-3">
-                                <Link to="#/" className="title text-dark h5">Walk In Testing</Link>
-                                <p className="text-muted mt-3">Walk into our testing facilities and get your results in person or at your convenience via email or whatsApp.</p>
-                                <Link to="#/" className="link">Read More <i className="ri-arrow-right-line align-middle"></i></Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="section about-section font-size-15 padding-bottom-20">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12">
+            <div className="section-title mb-4 pb-2 text-center">
+              <h4 className="choose-title mb-4 text-orange">
+                COVID Testing Services
+              </h4>
+              <p className="text-muted mx-auto para-desc mb-0">
+                We perform advanced Polymerase Chain Reaction (PCR) and
+                PCR-SARS-CoV-2 rapid diagnostic testing on travelers, the
+                general public, and on behalf of private institutions.
+              </p>
             </div>
-        </section>
-    )
-}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xl-4 col-md-4 col-12 mt-5">
+            <div className="card features feature-primary border-0">
+              <div className="icon text-center rounded-md icon-font">
+                <AiOutlineSafetyCertificate />
+              </div>
+              <div className="card-body p-0 mt-3">
+                <Link to="#/" className="title text-dark h5">
+                  Flying Certificate
+                </Link>
+                <p className="text-muted mt-3">
+                  Get fast, accurate results and your flying certificate with
+                  enough time to spare for your flight.
+                </p>
+                <Link to="#/" className="link">
+                  Read More <i className="ri-arrow-right-line align-middle"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
 
-export default CovidContent
+          <div className="col-xl-4 col-md-4 col-12 mt-5">
+            <div className="card features feature-primary border-0">
+              <div className="icon text-center rounded-md icon-font">
+                <SiPytest />
+              </div>
+              <div className="card-body p-0 mt-3">
+                <Link to="#/" className="title text-dark h5">
+                  PCR Testing
+                </Link>
+                <p className="text-muted mt-3">
+                  The gold standard of covid-19 testing with a high level of
+                  professionalism and expertise. Come and get to know your
+                  results.
+                </p>
+                <Link to="#/" className="link">
+                  Read More <i className="ri-arrow-right-line align-middle"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-md-4 col-12 mt-5">
+            <div className="card features feature-primary border-0">
+              <div className="icon text-center rounded-md icon-font">
+                <RiServiceLine />
+              </div>
+              <div className="card-body p-0 mt-3">
+                <Link to="#/" className="title text-dark h5">
+                  Same Day Service
+                </Link>
+                <p className="text-muted mt-3">
+                  Need to travel today? Get your results and certificate to fly
+                  on the same day before your flight!
+                </p>
+                <Link to="#/" className="link">
+                  Read More <i className="ri-arrow-right-line align-middle"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-md-4 col-12 mt-5">
+            <div className="card features feature-primary border-0">
+              <div className="icon text-center rounded-md icon-font">
+                <RiHomeGearLine />
+              </div>
+              <div className="card-body p-0 mt-3">
+                <Link to="#/" className="title text-dark h5">
+                  Home Testing Service
+                </Link>
+                <p className="text-muted mt-3">
+                  How convenient can it get? We come to your home, take your
+                  sample and send you the results. Sweet!
+                </p>
+                <Link to="#/" className="link">
+                  Read More <i className="ri-arrow-right-line align-middle"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-md-4 col-12 mt-5">
+            <div className="card features feature-primary border-0">
+              <div className="icon text-center rounded-md icon-font">
+                <RiHotelBedLine />
+              </div>
+              <div className="card-body p-0 mt-3">
+                <Link to="#/" className="title text-dark h5">
+                  Hotel Testing Service
+                </Link>
+                <p className="text-muted mt-3">
+                  Schedule your covid-19 test with the front desk in the
+                  convenience of your hotel room and we will come to you !
+                </p>
+                <Link to="#/" className="link">
+                  Read More <i className="ri-arrow-right-line align-middle"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-4 col-md-4 col-12 mt-5">
+            <div className="card features feature-primary border-0">
+              <div className="icon text-center rounded-md icon-font">
+                <FaWalking />
+              </div>
+              <div className="card-body p-0 mt-3">
+                <Link to="#/" className="title text-dark h5">
+                  Walk In Testing
+                </Link>
+                <p className="text-muted mt-3">
+                  Walk into our testing facilities and get your results in
+                  person or at your convenience via email or whatsApp.
+                </p>
+                <Link to="#/" className="link">
+                  Read More <i className="ri-arrow-right-line align-middle"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CovidContent;
